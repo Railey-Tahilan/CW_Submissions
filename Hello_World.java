5ac233249127e1d8df68c1868de7a1af24ec3fb0
Railey-Tahilan <railey0507@gmail.com>
2024-08-14 10:30:22 -1000

    Create Hello_World.java

---
+++ Hello_World.java
@@ -0,0 +1,21 @@
+/*
+ * This code is for the very first assignment and  I  will print out words and  submit it
+ */
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//TASK 1: Print out "Hello World"
+		System.out.print("Hello World!");
+		
+
+		//TASK 2: Print out "Hello World" and then print it again on the next line	
+
+		System.out.println(" Hello World!");
+		System.out.println("Hello World!");
+
+	}
+
+}
+
